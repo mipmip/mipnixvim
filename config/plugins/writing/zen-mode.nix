894d3d7ba5aa67ed3235f00f95f590d1c00a9a5c
Pim Snel <post@pimsnel.com>
2025-11-19 22:14:03 +0100

    add linny, scimark, zenmode, some mods

---
+++ config/plugins/writing/zen-mode.nix
@@ -0,0 +1,46 @@
+{
+  plugins.zen-mode = {
+    enable = true;
+    settings = {
+      # lifted from https://nix-community.github.io/nixvim/plugins/zen-mode/settings/index.html
+      plugins = {
+        #bufferline.enabled = true; # doesn't work for some reason
+        gitsigns.enabled = false;
+        tmux.enabled = false;
+        options = {
+          enabled = true;
+          ruler = false;
+          showcmd = false;
+        };
+        #twilight.enabled = true;
+      };
+      window = {
+        backdrop = 1;
+        height = 0.9;
+        options = {
+          number = false;
+          signcolumn = "no";
+          cursorline = false;
+          cursorcolumn = true;
+          foldcolumn = "0";
+        };
+        width = 0.8;
+      };
+    };
+
+    # twilight.enable = true; ## why is this here? zen mode has no twilight
+  };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>Z";
+      action = "<Cmd>ZenMode<CR>";
+      options = {
+        silent = true;
+        desc = "Zen-mode toggle";
+      };
+    }
+  ];
+
+}
