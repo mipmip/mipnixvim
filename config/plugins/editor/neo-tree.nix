e99f1c2f27f9f5fe8d1091cf43881f1cbb66f04b
Pim Snel <post@pimsnel.com>
2025-12-11 21:00:40 +0100

    fix extra copy paths menu in neotree

--- config/plugins/editor/neo-tree.nix
+++ config/plugins/editor/neo-tree.nix
@@ -1,3 +1,4 @@
+{ helpers, ... }:
 {
   plugins.neo-tree = {
     enable = true;
@@ -9,46 +10,45 @@
     ];
     addBlankLineAtTop = false;
 
-    #window = {
-    #  mappings = {
-    #    "s" = "split_with_window_picker";
-    #    #        "Y" = ''
-    #    #          function(state)
-    #    #             -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
-    #    #             -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
-    #    #             local node = state.tree:get_node()
-    #    #             local filepath = node:get_id()
-    #    #             local filename = node.name
-    #    #             local modify = vim.fn.fnamemodify
-    #    #
-    #    #             local results = {
-    #    #               filepath,
-    #    #               modify(filepath, ':.'),
-    #    #               modify(filepath, ':~'),
-    #    #               filename,
-    #    #               modify(filename, ':r'),
-    #    #               modify(filename, ':e'),
-    #    #             }
-    #    #
-    #    #             -- absolute path to clipboard
-    #    #             local i = vim.fn.inputlist({
-    #    #               'Choose to copy to clipboard:',
-    #    #               '1. Absolute path: ' .. results[1],
-    #    #               '2. Path relative to CWD: ' .. results[2],
-    #    #               '3. Filename: ' .. results[4],
-    #    #             })
-    #    #
-    #    #             if i > 0 then
-    #    #               local result = results[i]
-    #    #               if not result then return print('Invalid choice: ' .. i) end
-    #    #               os.execute('echo ' .. result .. '| xclip')
-    #    #               vim.notify('Copied: ' .. result)
-    #    #             end
-    #    #           end
-    #    #        '';
+    window = {
+      mappings = {
+        "Y" = helpers.mkRaw ''
+          function(state)
+             -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+             -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+             local node = state.tree:get_node()
+             local filepath = node:get_id()
+             local filename = node.name
+             local modify = vim.fn.fnamemodify
 
-    #  };
-    #};
+             local results = {
+               filepath,
+               modify(filepath, ':.'),
+               modify(filepath, ':~'),
+               filename,
+               modify(filename, ':r'),
+               modify(filename, ':e'),
+             }
+
+             -- absolute path to clipboard
+             local i = vim.fn.inputlist({
+               'Choose to copy to clipboard:',
+               '1. Absolute path: ' .. results[1],
+               '2. Path relative to CWD: ' .. results[2],
+               '3. Filename: ' .. results[4],
+             })
+
+             if i > 0 then
+               local result = results[i]
+               if not result then return print('Invalid choice: ' .. i) end
+               os.execute('echo ' .. result .. '| xclip')
+               vim.notify('Copied: ' .. result)
+             end
+           end
+        '';
+
+      };
+    };
 
     filesystem = {
       bindToCwd = false;
